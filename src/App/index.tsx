f9825946e869720dcaa52125416332b4583f60b5
Yuri Galvão <yuri.gvao@gmail.com>
2025-02-19 18:43:21 -0300

    create app component

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import ExpenseComponent from '../components/ExpenseComponent'
+import styles from './styles.module.css'
+
+function App() {
+  return (
+    <main className={styles.app}>
+      <ExpenseComponent />
+    </main>
+  )
+}
+
+
+export default App
